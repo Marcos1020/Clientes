61f11f907fb61d3d60f5951ac94c6f2953581c1b
tqi_mcampos <tqi_mcampos@uolinc.com>
2022-06-13 23:31:38 -0300

    Implementando os testes Unitarios e fazendo alterações necessarias para o desenvolvimento do projeto

--- src/main/java/com/testes/Clientes/Controller/UserController.java
+++ src/main/java/com/testes/Clientes/Controller/UserController.java
@@ -7,6 +7,7 @@ import com.testes.Clientes.exception.NotFoundException;
 import com.testes.Clientes.exception.ObjectAlreadyExists;
 import com.testes.Clientes.exception.PreconditionFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,22 +19,28 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Value("${access.token}")
+    private String token;
+
     public UserController(){
         this.userService = userService;
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse>newUser(@RequestBody UserRequest usuarioRequest) throws PreconditionFailedException {
+    public ResponseEntity<UserResponse>newUser(@RequestBody UserRequest usuarioRequest, @RequestHeader String accessToken) throws PreconditionFailedException {
+        if(!accessToken.equals(token)){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         UserResponse response = this.userService.newUser(usuarioRequest);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
 
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<UserResponse>search(
-
-            @PathVariable("id") int idUsuario) throws NotFoundException{
-
+    public ResponseEntity<UserResponse>search(@PathVariable("id") int idUsuario,@RequestHeader String accessToken) throws NotFoundException{
+        if(!accessToken.equals(token)){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         final UserResponse userResponse = this.userService.searchById(idUsuario);
         return new ResponseEntity<>(userResponse,HttpStatus.OK);
 
@@ -41,14 +48,19 @@ public class UserController {
 
     @PutMapping("{id}")
     public ResponseEntity<UserResponse>userUpdate (
-            @PathVariable("id") int idUsuario, @RequestBody UserRequest userRequest) throws NotFoundException {
+            @PathVariable("id") int idUsuario, @RequestBody UserRequest userRequest,@RequestHeader String accessToken) throws NotFoundException {
+        if(!accessToken.equals(token)){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         final UserResponse userResponse = this.userService.updateUser(idUsuario, userRequest);
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> inativar(
-            @PathVariable("id") int idUsuario) throws ObjectAlreadyExists{
+    public ResponseEntity<Void> inativar(@PathVariable("id") int idUsuario,@RequestHeader String accessToken) throws ObjectAlreadyExists{
+        if(!accessToken.equals(token)){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         this.userService.inativar(idUsuario);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
